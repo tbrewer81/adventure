1ed0ac9c99820ee736255362f1006935ea5d7978
tbrewer81 <91998629+tbrewer81@users.noreply.github.com>
2022-12-29 13:04:23 -0600

    attempt 4

--- adventure.py
+++ adventure.py
@@ -2,23 +2,39 @@ import random
 import time
 
 
-prize = random.choice([
-" a million dollars",
-"scammed by hacker", 
-"one butterscotch candy",
-"a brand new Tesla", 
-"nothing"
-])
-prize = random.choice(prize)
+prizes = [
+  " a million dollars",
+  " being scammed by hacker",
+  " one butterscotch candy",
+  " a brand new Tesla",
+  " ...nothing"
+  ]
+
+
+prize = random.choice(prizes)
+
+
+g_prize = random.choice([
+  " a million dollars",
+  " one butterscotch candy",
+  " a brand new Tesla",
+  ])
+
+
+b_prize = random.choice([
+  " ...nothing",
+  " being scammed by hacker"
+  ])
 
 
 # print pause
-def print_pause(message, pause=2):
+def print_pause(message, pause=0.0):
     print(message)
     time.sleep(pause)
 
+
 # valid Input Function:
-def valid_input(prompt, choice1, choice2): 
+def valid_input(prompt, choice1, choice2):
     while True:
         response = input(prompt).lower()
         if response == choice1:
@@ -33,14 +49,13 @@ def valid_input(prompt, choice1, choice2):
 def intro():
     print_pause("Welcome to this very special game to Trick or Treat!")
     print_pause("Will you be tricked or treated?")
-    print_pause("I am excited to see what you will receive" )
+    print_pause("I am excited to see what you will receive")
     print_pause("Choose carefully")
     print_pause("Best of luck to you, but you must be on your way now.")
 
 
 def accept_game():
     response = valid_input("Do you wish to continue?", "yes", "no")
-    #accepting game play
     if response == "yes":
         print_pause("Lets begin...")
         prizes(prize)
@@ -49,32 +64,24 @@ def accept_game():
 
 
 def prizes(prize):
-    print_pause( "you can win" + prize +"!")
+    print_pause("you have a chance to win" + prize + "!")
     print_pause("will you get a trick or a treat?")
     response = valid_input("which would you like?", "treat", "trick")
-    # treat function
-    if response == "trick":
-        print_pause("yay!" , pause=2)
-        print_pause("lets go!")
-    # trick function
+    if response == 'trick':
+        bad_prize()
     else:
-        print_pause ("scammed by a hacker" + prize  + " prizes")
-        print_pause("lets go!")
+        good_prize()
 
 
-def a_million_dollars():
-    print_pause("You have opted for yearly payments!")
-    print_pause("please fill this out with you banking inforamtion")
-    print_pause("lets head over to the bank.")
-    print_pause( "Congratulations on your" + prize +"!")
-    def new_game():
+def good_prize():
+    print_pause("yay!")
+    print_pause("you got " + g_prize)
+    print_pause("Congratulations!")
 
 
-def scammed_by_a_hacker():
-    print_pause(" Sorry, but you have been hacked by a scammer")
-    print_pause("they stole all the money from your account")
-    print_pause("lets head over to the bank")
-    new_game()
+def bad_prize():
+    print_pause(" Sorry, but you got " + b_prize)
+    print_pause("better luck next time!")
 
 
 def new_game():
@@ -82,7 +89,14 @@ def new_game():
     if response == "yes":
         print_pause("ok! Lets begin!")
         global prize
-        prize = random.choice(prize)
+        prize = random.choice([
+          " a million dollars",
+          " being scammed by hacker",
+          " one butterscotch candy",
+          " a brand new Tesla",
+          " ...nothing"
+          ])
+        accept_game()
     else:
         print_pause("See you soon! Bye for now!")
 
@@ -91,5 +105,7 @@ def new_game():
 def play_prizes_game():
     intro()
     accept_game()
+    new_game()
+
 
 play_prizes_game()
